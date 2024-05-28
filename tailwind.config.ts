5404b534b9d5aee031ff07268ee9567296155e3a
André Silva <andre.silva@posteo.de>
2024-05-28 12:19:10 +1000

    Update tailwind config with color settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,33 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  presets:[require("@relume_io/relume-tailwind")],
+  presets: [require("@relume_io/relume-tailwind")],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        base: {
+          50: "",
+          // expand palette to contrast well with base content and with similar hue
+          100: "",
+          200: "",
+          300: "",
+        },
+        // backgrounds and H1
+        "base-content": "",
+
+        // to create text hierarchy
+        "base-content-secondary": "",
+
+        //CTA colors
+        primary: {
+          text: "",
+          "primary-content": "",
+        },
       },
     },
   },
