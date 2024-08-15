f0751c31f9a743daadad4c96566639b0c2bcd2b2
André Silva <andre.silva@posteo.de>
2024-08-15 15:30:04 +1000

    remove unnecessary import

--- src/app/teachers/page.tsx
+++ src/app/teachers/page.tsx
@@ -1,5 +1,5 @@
 import { Layout229, Layout229Defaults } from "@/components/teachers/Layout229";
-import { Layout38, Layout38Defaults } from "@/components/teachers/Layout38";
+
 import { Layout44,Layout44Defaults } from "@/components/teachers/Layout44";
 
 export default function Teachers(){
